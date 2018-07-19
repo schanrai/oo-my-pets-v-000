5036a34c92d5e0e5691726551648a1acb9d94332
Sushi Chanrai <sushi@pathworks.co>
2018-07-19 02:10:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -79,6 +79,18 @@ class Owner
     @pets = {fishes: [], cats: [], dogs: []}
   end
 
+=begin
+#alternative solution - neater to use .clear method on values
+  def sell_pets
+  pets.each do |species, animals|
+    animals.each do |animal|
+      animal.mood = "nervous"
+    end
+    animals.clear
+  end
+end
+=end
+
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
